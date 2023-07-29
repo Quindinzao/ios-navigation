03abfbb5e9679f5f63b186a727de91dd6f3ff4f3
quindinzao-dev <devjaodev@gmail.com>
2023-07-29 01:29:33 -0300

    first commit

--- NavigationSwiftUI/ContentView.swift
+++ NavigationSwiftUI/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Screen(
+//                title: "Home",
+                text: "HOME SCREEN",
+                imageName: "house",
+                color: .orange)
         }
-        .padding()
+    }
+}
+
+struct Screen: View {
+//    let title: String
+    let text: String
+    let imageName: String
+    let color: Color
+
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 64, height: 64, alignment: .center)
+                .padding(2)
+            Text(text)
+                .font(.system(size: 22, weight: .bold, design: .monospaced))
+            NavigationLink {
+                Screen(
+//                    title: "Notification",
+                    text: "NOTIFICATION SCREEN",
+                    imageName: "bell",
+                    color: .green)
+            } label: {
+                ContinueButton(color: color)
+            }
+
+        }
+//        .navigationTitle(title)
+    }
+}
+
+struct ContinueButton: View {
+    let color: Color
+    var body: some View {
+        Text("Continue")
+            .frame(maxWidth: .infinity - 24)
+            .frame(height: 54, alignment: .center)
+            .font(.system(size: 18, weight: .light, design: .monospaced))
+            .foregroundColor(.black)
+            .background(color)
+            .cornerRadius(8)
+            .padding(12)
     }
 }
 
