5689d29897cfce97ff180b451c22db29ed5e0ea0
quindinzao-dev <devjaodev@gmail.com>
2023-07-28 23:41:37 -0300

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by João Victor Fernandes on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
